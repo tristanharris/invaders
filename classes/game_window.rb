4d1b39dfedb49bdc2314dcce931c0120410dc8ec
Tristan Harris <tris.dev@pumpcottage.me.uk>
2012-11-15 18:29:13 +0000

    Show an image

--- classes/game_window.rb
+++ classes/game_window.rb
@@ -3,10 +3,16 @@ class GameWindow < Gosu::Window
   def initialize(width=640, height=480, fullscreen=false)
     super
     self.caption = 'Space Invaders'
+
+    @image = Gosu::Image.new(self, 'media/ship.png')
   end
 
   def update
     close if button_down? Gosu::Button::KbEscape
+  end
+
+  def draw
+    @image.draw(100,100,1)
   end
 
 end
